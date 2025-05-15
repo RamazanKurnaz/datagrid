123ceebb908e93b3ca4801785eeab2ebac8bd83a
RamazanKurnaz <ramazankurnaz74@hotmail.com>
2025-05-15 14:25:52 +0300

    chore: build hataları geçici ignore edildi

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  },
   plugins: [react()]
 })
